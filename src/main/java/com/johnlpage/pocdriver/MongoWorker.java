6e043b8f8d58078d93b2a85c5b96ada264fe0919
John Zheng <john.zheng@hp.com>
2021-01-21 19:31:34 +0800

    add obj2 ...

--- src/main/java/com/johnlpage/pocdriver/MongoWorker.java
+++ src/main/java/com/johnlpage/pocdriver/MongoWorker.java
@@ -393,14 +393,14 @@ public class MongoWorker implements Runnable {
         query2.append("_id", new Document("$in", idList)); //org.bson.Document
 		
         MongoCursor<Document> cursor2;
-        cursor2 = mongoClient.getDatabase("POCDB").getCollection("clients_type").find(query).iterator();
+        cursor2 = mongoClient.getDatabase("POCDB").getCollection("clients_type").find(query2).iterator();
         while (cursor2.hasNext()) {
             @SuppressWarnings("unused")
-            Document obj = cursor2.next();
+            Document obj2 = cursor2.next();
             //System.out.println(obj);
-            System.out.println("_id=" + obj.getString("_id"));
+            System.out.println("_id=" + obj2.getString("_id"));
         }
-        cursor.close();
+        cursor2.close();
         //System.out.println(mongoClient.getDatabase("POCDB").getCollection("clients_type").find(query));
 		
         //System.out.println(mongoClient.getDatabase("POCDB").getCollection("clients_type").find());
